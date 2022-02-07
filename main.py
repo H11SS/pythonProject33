2d03a36d8f542d0cf06078e7459fd6999ad2b0fb
H11SS <93755550+H11SS@users.noreply.github.com>
2022-02-07 15:51:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+class CalUtils:
+    heights = 0
+    names = 0
+
+    totalStudentHeight = 0
+    totalStudentCount = 0
+
+def readData(self):
+    file = open("listOfStudentHeight.txt", mode='r', encoding='utf-8')
+    data = file.readlines()
+    print("dara:",data)
+
+
+
+
+
